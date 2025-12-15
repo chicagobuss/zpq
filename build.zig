cd6b914b8a695b7fd176d0314122a7c0ff5d5d5f
Joshua Buss <joshua@skyway.cloud>
2025-12-15 15:22:08 -0800

    build: Restore 'test' step and ensure all tests run

--- build.zig
+++ build.zig
@@ -35,4 +35,14 @@ pub fn build(b: *std.Build) void {
 
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
+
+    // Unit Tests
+    const lib_unit_tests = b.addTest(.{
+        .root_module = zpq_mod,
+    });
+
+    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
+
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_lib_unit_tests.step);
 }
--- src/zpq.zig
+++ src/zpq.zig
@@ -3,3 +3,8 @@ pub const schema = @import("zpq/schema.zig");
 pub const column = @import("zpq/column.zig");
 pub const decoder = @import("zpq/decoder.zig");
 pub const rle = @import("zpq/rle.zig");
+
+test {
+    _ = @import("zpq/thrift_test.zig");
+    _ = rle;
+}
